0e70129f1ce2877b0aa542d589cf3d3cd19e2467
eya kabech <eyakabech@MacBook-Air-de-eya.local>
2025-06-14 16:48:56 +0100

    Supprime le sous-module et ajoute AnimalShelter comme dossier normal

---
+++ AnimalShelter/server/server.js
@@ -0,0 +1,32 @@
+const express = require("express"); 
+const app = express();  
+const cors = require("cors");
+const cookies = require("cookie-parser");
+
+require('dotenv').config();
+require("./config/mongoose.config");
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookies())
+
+app.use(cors({
+    credentials: true, origin:"http://localhost:3000"
+}));
+
+// Add route
+require("../server/routes/users")(app)
+require("../server/routes/pets")(app)
+require("../server/routes/reports")(app)
+require("../server/routes/products")(app)
+
+const server = app.listen(8000, ()=>console.log("Listening on Port 8000"))             
+//socket.io
+const io = require("socket.io")(server,{cors:true})
+
+io.on('connection',socket =>{
+    console.log(socket.id)
+})
+ 
+
